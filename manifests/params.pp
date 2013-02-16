afd6eeeab52e5358599e0bd925be523ea1f14c00
Alessandro Franceschi <al@lab42.it>
2013-02-17 00:22:00 +0100

    Example42-tools/module_clone.sh -t package42 -n zip

---
+++ manifests/params.pp
@@ -0,0 +1,29 @@
+# Class: zip::params
+#
+# This class defines default parameters used by the main module class zip
+# Operating Systems differences in names and paths are addressed here
+#
+# == Variables
+#
+# Refer to zip class for the variables defined here.
+#
+# == Usage
+#
+# This class is not intended to be used directly.
+# It may be imported or inherited by other classes
+#
+class zip::params {
+
+  ### Application related parameters
+
+  $package = $::operatingsystem ? {
+    default => 'zip',
+  }
+
+  # General Settings
+  $my_class = ''
+  $version = 'present'
+  $absent = false
+  $noops = false
+
+}
